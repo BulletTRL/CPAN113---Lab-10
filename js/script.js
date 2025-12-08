ac9efae1cb5bc67c87688480ced755e8b8fa2b79
Tyler (Bullet) Hoyos <tylerhoyos2@gmail.com>
2025-12-08 18:24:05 -0500

    Test Script connection and output logging

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Basic Setup
+
+console.log("Lab 10 scripts have been loaded");
+
+const output = document.getElementById("output");
+const log = (msg) => (output.innerHTML += `<p>${msg}</p>`);
+
+log("<strong>Phase 1 Setup has been Completed:</strong> HTML, CSS, and JavaScript linked successfully");
