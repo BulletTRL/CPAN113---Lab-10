86ecc8f7eb758126ff1ffe861a707277b03ff859
Tyler (Bullet) Hoyos <tylerhoyos2@gmail.com>
2025-12-08 20:28:04 -0500

    Add subclass PerishableProductProperties definition

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ const output = document.getElementById("output");
 const log = (msg) => (output.innerHTML += `<p>${msg}</p>`);
 
 log("<strong>Phase 1 Setup has been Completed:</strong> HTML, CSS, and JavaScript linked successfully");
+
+// Subclass Inharitance
+
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);  // call parent constructor
+        this.expirationDate = expirationDate;
+    }
+
+    // Override parent method
+    toString() {
+        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+    }
+}
